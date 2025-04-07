be4f56ddfa2c6feb25c9fc76ec29e114f99ac821
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2025-04-07 00:28:07 -0500

    :zap: Added analytics :zap:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/tailwind.css'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased">
+      <Analytics />
       <head>
         <link
           rel="preconnect"
